f9cb206461fa87bade50b7f77fe038516a03c5aa
Osvaldo Torasso <thorium232@users.noreply.github.com>
2022-02-18 16:29:12 -0300

    Update HelloWorld.java

--- src/main/java/th/misc/HelloWorld.java
+++ src/main/java/th/misc/HelloWorld.java
@@ -4,6 +4,6 @@ public class HelloWorld {
 
   public static void main(String[] args) {
     System.out.println("Hello World!");
-    System.out.println("Gonza se la come reprocessing!");
+    System.out.println("Gonza es un capo!");
   }
 }

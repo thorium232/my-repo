27460da628763ab6b1ad1598733ef145cc08f34b
Osvaldo Torasso <thorium232@users.noreply.github.com>
2022-09-08 21:10:19 -0300

    Change test

--- src/main/java/th/misc/Another.java
+++ src/main/java/th/misc/Another.java
@@ -5,5 +5,9 @@ public class Another {
   public static void main(String[] args) {
     // Add a change
     System.out.println("Added file!");
+    String s = "";
+    for (int i = 0; i < 10; i++) {
+      s = s + i;
+    }
   }
 }
